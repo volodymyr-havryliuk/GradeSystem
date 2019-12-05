4574cd63aff56409addf6e8cf0ad5565edfd112e
craviee <danielcraviee@gmail.com>
2019-12-05 13:53:58 +0100

    Adding more attributes to grade structure and updating tests

--- tests/test_grade_system.py
+++ tests/test_grade_system.py
@@ -15,17 +15,17 @@ class GradesSystemTest(unittest.TestCase):
         self.assertEqual(len(self.student.grades), 0, "Student should NOT have a grade initially")
 
     def test_students_grade_is_not_validate(self, grade=3):
-        self.professor.put_grade(grade, self.student)
+        self.professor.put_grade("SDM", 20192, grade, self.student)
         self.assertFalse(self.student.grades[len(self.student.grades)-1]['valid'], "Student should NOT have a validated grade")
 
     def test_grade_has_value_after_professor_put_grade(self, grade=5):
-        self.professor.put_grade(grade, self.student)
+        self.professor.put_grade("SDM", 20192, grade, self.student)
         self.assertEqual(len(self.student.grades), 1, "Student should have one grade")
         self.assertEqual(self.student.grades[len(self.student.grades)-1]['value'], grade, "Student should have the grade with value " + str(grade))
         self.assertFalse(self.student.is_grade_validated(index=0), "Student grade should NOT have validated grade")
 
     def test_grade_is_validated_after_validation(self, grade=5):
-        self.professor.put_grade(grade, self.student)
+        self.professor.put_grade("SDM", 20192, grade, self.student)
         self.dean_office_employee.validate_grade(self.student, index=len(self.student.grades)-1)
         self.assertEqual(len(self.student.grades), 1, "Student should have one grade")
         self.assertEqual(self.student.grades[len(self.student.grades)-1]['value'], grade, "Student should have the grade with value " + str(grade))
@@ -33,7 +33,7 @@ class GradesSystemTest(unittest.TestCase):
 
     def test_student_has_multiple_grades(self, number_grades=3, grade=4):
         for i in range(number_grades):
-            self.professor.put_grade(grade, self.student)
+            self.professor.put_grade("SDM", 20192, grade, self.student)
         self.assertEqual(len(self.student.grades), number_grades, "Student should have "+ str(number_grades)+ " grades")
         for i in range(number_grades):
             self.assertFalse(self.student.is_grade_validated(index=i), "Student grade should NOT have validated grade")
@@ -42,7 +42,7 @@ class GradesSystemTest(unittest.TestCase):
         self.assertTrue(index_valid_grade>=0 and index_valid_grade < number_grades, "Wrong test parameters")
 
         for i in range(number_grades):
-            self.professor.put_grade(grade, self.student)
+            self.professor.put_grade("SDM", 20192, grade, self.student)
         self.assertEqual(len(self.student.grades), number_grades, "Student should have "+ str(number_grades)+ " grades")
         self.dean_office_employee.validate_grade(self.student, index=index_valid_grade)
         for i in range(number_grades):
@@ -53,7 +53,7 @@ class GradesSystemTest(unittest.TestCase):
 
     def test_dean_office_validate_all_grades(self, number_grades=3, grade=4):
         for i in range(number_grades):
-            self.professor.put_grade(grade, self.student)
+            self.professor.put_grade("SDM", 20192, grade, self.student)
         for i in range(number_grades):
             self.dean_office_employee.validate_grade(self.student, index=i)
         for i in range(number_grades):
@@ -62,7 +62,7 @@ class GradesSystemTest(unittest.TestCase):
     def test_dean_office_fail_validate_grade(self, grade=1):
         self.assertTrue(grade > 5 or grade < 2, "Wrong test parameters")
 
-        self.professor.put_grade(grade, self.student)
+        self.professor.put_grade("SDM", 20192, grade, self.student)
         self.dean_office_employee.validate_grade(self.student, index=len(self.student.grades)-1)
         self.assertFalse(self.student.is_grade_validated(index=len(self.student.grades)-1), "Student grade should NOT have validated grade")
 
